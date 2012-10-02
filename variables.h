da6d19706da39b893d7f930c36fbfc2b4c85fc5b
Juan Gonzalez <0505gonzalez@gmail.com>
2012-10-02 00:44:30 -0700

    removed header info

--- variables.h
+++ variables.h
@@ -1,8 +1,3 @@
-/*****************************************************************************/
-/* This is the program skeleton for homework 2 in CS 184 by Ravi Ramamoorthi */
-/* Extends HW 1 to deal with shading, more transforms and multiple objects   */
-/*****************************************************************************/
-
 // This is the basic include file for the global variables in the program.  
 // Since all files need access to it, we define EXTERN as either blank or 
 // extern, depending on if included in the main program or not.  
